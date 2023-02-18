1fb88a9044d2d7c27515b75d59c163ef63287a53
Felix <ftjb1@st-andrews.ac.uk>
2023-02-18 02:13:41 +0000

    Guess Tests Added.

--- src/test/java/stacs/wordle/WordleAppTest.java
+++ src/test/java/stacs/wordle/WordleAppTest.java
@@ -43,6 +43,7 @@ public class WordleAppTest
         assertTrue(app.getWordList().contains(app.getWordle()));
     }
 
+    // Guess Colour Result Tests
     @Test
     public void allGreys()
     {
@@ -52,5 +53,169 @@ public class WordleAppTest
         app.processGuess(guess, 0);
         assertEquals(app.getResultForGuess(0), greys);
     }
+
+    // Anagram Test
+    @Test
+    public void allYellows()
+    {
+        Result[] yellows = {Result.YELLOW, Result.YELLOW, Result.YELLOW, Result.YELLOW, Result.YELLOW}; 
+        app.setWordle("space");
+        String guess = "paces";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), yellows);
+    }
+
+    @Test
+    public void allGreens()
+    {
+        Result[] greens = {Result.GREEN, Result.GREEN, Result.GREEN, Result.GREEN, Result.GREEN};
+        app.setWordle("lunch");
+        String guess = "lunch";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), greens);
+    }
+
+
+    // we need to ensure that both the first and last 'e' in our guess attain result GREEN
+    // and that the second 'e' in our guess does not rematch to either of the e's in 'eagle'
+    @Test
+    public void duplicateLetterNotRematched()
+    {
+        Result[] expected = {Result.GREEN, Result.GREY, Result.GREY, Result.GREY, Result.GREEN}; 
+        app.setWordle("eagle");
+        String guess = "eerie";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    // mix of greens and yellows only
+    @Test
+    public void anagramTest1()
+    {
+        Result[] expected = {Result.GREEN, Result.GREEN, Result.GREEN, Result.YELLOW, Result.YELLOW}; 
+        app.setWordle("angle");
+        String guess = "angel";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    @Test
+    public void anagramTest2()
+    {
+        Result[] expected = {Result.YELLOW, Result.YELLOW, Result.YELLOW, Result.GREEN, Result.GREEN}; 
+        app.setWordle("elbow");
+        String guess = "below";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    @Test
+    public void anagramTest3()
+    {
+        Result[] expected = {Result.YELLOW, Result.YELLOW, Result.GREEN, Result.YELLOW, Result.YELLOW}; 
+        app.setWordle("scalp");
+        String guess = "claps";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+
+    // Mixes of greens, yellows, and greys
+    @Test
+    public void mixedTest1()
+    {
+        Result[] expected = {Result.YELLOW, Result.GREEN, Result.GREY, Result.GREY, Result.GREY}; 
+        app.setWordle("range");
+        String guess = "naval";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    @Test
+    public void mixedTest2()
+    {
+        Result[] expected = {Result.YELLOW, Result.GREEN, Result.GREY, Result.YELLOW, Result.GREY}; 
+        app.setWordle("range");
+        String guess = "navel";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    @Test
+    public void mixedTest3()
+    {
+        Result[] expected = {Result.YELLOW, Result.GREEN, Result.GREY, Result.GREY, Result.GREY}; 
+        app.setWordle("orbit");
+        String guess = "track";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    // StudRes tests
+    @Test
+    public void studResTest1()
+    {
+        Result[] expected = {Result.GREY, Result.GREY, Result.YELLOW, Result.GREY, Result.GREY}; 
+        app.setWordle("ounce");
+        String guess = "dream";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    @Test
+    public void studResTest2()
+    {
+        Result[] expected = {Result.GREY, Result.GREY, Result.GREY, Result.GREY, Result.YELLOW}; 
+        app.setWordle("ounce");
+        String guess = "radio";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    @Test
+    public void studResTest3()
+    {
+        Result[] expected = {Result.GREY, Result.GREY, Result.GREY, Result.GREY, Result.GREEN}; 
+        app.setWordle("ounce");
+        String guess = "table";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    @Test
+    public void studResTest4()
+    {
+        Result[] expected = {Result.GREY, Result.YELLOW, Result.GREY, Result.GREEN, Result.GREY}; 
+        app.setWordle("ounce");
+        String guess = "snack";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+    @Test
+    public void studResTest5()
+    {
+        Result[] expected = {Result.YELLOW, Result.YELLOW, Result.GREY, Result.GREY, Result.GREEN}; 
+        app.setWordle("ounce");
+        String guess = "noise";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+
+    @Test
+    public void studResTest6()
+    {
+        Result[] expected = {Result.GREEN, Result.GREEN, Result.GREEN, Result.GREEN, Result.GREEN}; 
+        app.setWordle("ounce");
+        String guess = "ounce";
+        app.processGuess(guess, 0);
+        assertEquals(app.getResultForGuess(0), expected);
+    }
+
+   
+
+
+
 
 }
